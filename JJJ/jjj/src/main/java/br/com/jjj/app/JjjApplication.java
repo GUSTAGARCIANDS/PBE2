575e96bdccfce67862296c42ec79ae724ee760f2
GUSTAGARCIANDS <159182569+GUSTAGARCIANDS@users.noreply.github.com>
2025-02-18 07:30:03 -0300

    Add files via upload

---
+++ JJJ/jjj/src/main/java/br/com/jjj/app/JjjApplication.java
@@ -0,0 +1,13 @@
+package br.com.jjj.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjjApplication.class, args);
+	}
+
+}
